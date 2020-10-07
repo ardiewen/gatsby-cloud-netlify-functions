8586124d8e3e94bf42b928e65ff34da46d08ea32
Ardie Wen <ardie.wen@hunterlily.co>
2020-10-07 19:50:10 +0000

    feat: test custom functions folder

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,33 @@
  */
 
 // You can delete this file if you're not using it
+const glob = require("glob")
+const path = require("path")
+const fs = require("fs")
+
+exports.onPostBuild = () => {
+  // Configure where the functions are kept and where we want to move them.
+  const srcLocation = `${__dirname}/src/functions`
+  const outputLocation = `${__dirname}/public/functions`
+
+  if (!fs.existsSync(outputLocation)) {
+    fs.mkdirSync(outputLocation)
+  }
+
+  // Get all the functions.
+  const modules = glob.sync("*.js", { cwd: srcLocation })
+  modules.forEach(src => {
+    const moduleSrc = path.join(srcLocation, src)
+    const moduleOut = path.join(
+      outputLocation,
+      path.basename(src, path.extname(src)) + ".js"
+    )
+
+    // Copy file to new location.
+    fs.copyFile(moduleSrc, moduleOut, err => {
+      if (err) {
+        throw err
+      }
+    })
+  })
+}
