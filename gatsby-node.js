d8e2a8997ef8e3f9d49fa88bdbf95b431f6270ac
Ardie Wen <ardie.wen@hunterlily.co>
2020-10-08 15:33:01 +0000

    test: gatsby support onpostbuild netlify-lambda

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,29 +9,46 @@ const glob = require("glob")
 const path = require("path")
 const fs = require("fs")
 
-exports.onPostBuild = () => {
-  // Configure where the functions are kept and where we want to move them.
-  const srcLocation = `${__dirname}/src/functions`
-  const outputLocation = `${__dirname}/public/functions`
+const util = require("util")
+const exec = util.promisify(require("child_process").exec)
 
-  if (!fs.existsSync(outputLocation)) {
-    fs.mkdirSync(outputLocation)
+exports.onPostBuild = async () => {
+  const buildFunctions = async () => {
+    const { stdout, stderr } = await exec("yarn run lambda")
+    if (stderr) {
+      console.log(`stderr: ${stderr}`)
+      return
+    }
+    if (stdout) {
+      console.log(`stdout: ${stdout}`)
+    }
   }
-
-  // Get all the functions.
-  const modules = glob.sync("*.js", { cwd: srcLocation })
-  modules.forEach(src => {
-    const moduleSrc = path.join(srcLocation, src)
-    const moduleOut = path.join(
-      outputLocation,
-      path.basename(src, path.extname(src)) + ".js"
-    )
-
-    // Copy file to new location.
-    fs.copyFile(moduleSrc, moduleOut, err => {
-      if (err) {
-        throw err
-      }
-    })
-  })
+  await buildFunctions()
 }
+
+// exports.onPostBuild = () => {
+//   // Configure where the functions are kept and where we want to move them.
+//   const srcLocation = `${__dirname}/src/functions`
+//   const outputLocation = `${__dirname}/public/functions`
+
+//   if (!fs.existsSync(outputLocation)) {
+//     fs.mkdirSync(outputLocation)
+//   }
+
+//   // Get all the functions.
+//   const modules = glob.sync("*.js", { cwd: srcLocation })
+//   modules.forEach(src => {
+//     const moduleSrc = path.join(srcLocation, src)
+//     const moduleOut = path.join(
+//       outputLocation,
+//       path.basename(src, path.extname(src)) + ".js"
+//     )
+
+//     // Copy file to new location.
+//     fs.copyFile(moduleSrc, moduleOut, err => {
+//       if (err) {
+//         throw err
+//       }
+//     })
+//   })
+// }
