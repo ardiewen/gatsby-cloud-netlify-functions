5da172d84d7ba830b7ce3b6a4ac04639392ed386
Ardie Wen <ardie.wen@hunterlily.co>
2020-10-26 20:09:12 +0000

    feat: additional testing scenarios

--- src/functions/test2/index.js
+++ src/functions/test2/index.js
@@ -1,6 +1,6 @@
 exports.handler = function (event, context, callback) {
   callback(null, {
     statusCode: 200,
-    body: "Hello, World using test.js",
+    body: "Hello, World using index.js",
   })
 }
